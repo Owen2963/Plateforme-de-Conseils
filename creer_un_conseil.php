e36132fde61126093113dade4cbb8ae9986c8149
Wazen12 <87486197+Wazen12@users.noreply.github.com>
2024-05-23 23:38:18 +0200

    Add files via upload

---
+++ creer_un_conseil.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+$nom_dossier = "Articles/";
+
+// Créer le dossier
+if (!file_exists($nom_dossier)) {
+    mkdir($nom_dossier);
+    echo "Le dossier $nom_dossier a été créé.";
+}
+
+
+if (isset($_POST['submit'])) {
+    // Vérifier si le champ de texte n'est pas vide
+
+    if (!empty($_POST['content'])) {
+        $titre = $_POST['title'];
+
+        
+        // Générer un nom de fichier unique basé sur la date et l'heure actuelles
+        $filename = $nom_dossier."Article_". $titre ."_". date("Ymd_His") . ".txt";
+        
+        // Récupérer le contenu du champ de texte
+        $content = $_POST['content'];
+        
+        // Écrire le contenu dans le fichier texte
+        file_put_contents($filename, $content);
+        
+        echo "Le document texte a été créé avec succès !";
+       echo "<script>setTimeout(function(){window.location.href='index.html';}, 100);</script>";
+
+        
+       
+    } else {
+        echo "Veuillez saisir du texte pour créer le document.";
+       
+    }
+}
+?>
+
