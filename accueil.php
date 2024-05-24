2d1f988a17c8cb80c2b5b5dc35fa491f5215dde7
Owen2963 <130049264+Owen2963@users.noreply.github.com>
2024-05-24 15:53:29 +0200

    Update and rename index.html to accueil.php

--- accueil.php
+++ accueil.php
@@ -14,7 +14,7 @@
 				<li class="page_actuel"><b><a href="accueil.html">Accueil</a></b></li>
 				<li><b><a href="nos_conseils.html">Nos Conseils</a></b></li>
 				<li><b><a href="conseil_du_jour.html">Conseil du jour</a></b></li>
-				<li><b><a href="creer_un_conseil.html">Créer un conseil</a></b></li>
+				<li><b><a href="creer_un_conseil_bis.php">Créer un conseil</a></b></li>
 				<li><b><a href="profil.html">Profil</a></b></li>
 			</ul>
 		</nav>
