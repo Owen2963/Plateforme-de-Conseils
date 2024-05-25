d3d2270fe3229e6d0f6ec45f85f4779d5932d56c
_Navire_ <132008539+verifiedNavire@users.noreply.github.com>
2024-05-25 11:16:50 +0200

    Update creer_un_conseil_bis.php

--- creer_un_conseil_bis.php
+++ creer_un_conseil_bis.php
@@ -10,10 +10,10 @@
     <h1 class="titre">Site de conseils de la vie quotidienne</h1>
     <nav class="navbar">
         <ul>
-            <li class="page_actuel"><b><a href="accueil.php">Accueil</a></b></li>
+            <li><b><a href="accueil.php">Accueil</a></b></li>
             <li><b><a href="nos_conseils.html">Nos Conseils</a></b></li>
             <li><b><a href="conseil_du_jour.html">Conseil du jour</a></b></li>
-            <li><b><a href="creer_un_conseil_bis.php">Créer un conseil</a></b></li>
+            <li class="page_actuel"><b><a href="creer_un_conseil_bis.php">Créer un conseil</a></b></li>
             <li><b><a href="profil.html">Profil</a></b></li>
         </ul>
     </nav>
