61e933c435f1981484bb3ef534797c510a41d73e
Owen2963 <130049264+Owen2963@users.noreply.github.com>
2024-05-26 10:41:56 +0200

    Update logout.php

--- Version2/logout.php
+++ Version2/logout.php
@@ -1,8 +1,17 @@
 <?php
+// Reprend la session existante
 session_start();
+
+// Supprime toutes les variables de session
 session_unset();
+
+// Détruit la session actuelle
 session_destroy();
+
+// Supprime le cookie en définissant son expiration à une heure passée
 setcookie("user", "", time() - 3600, "/");
+
+// Redirige l'utilisateur vers la page d'accueil
 header('Location: index.php');
-exit;
+exit; // Arrête l'exécution du script
 ?>
